812bdf86225cb5f997665fe126e8e5ae3811a367
kran891 <kranthi8919@gmail.com>
2023-12-11 20:24:57 +0530

    movieservices

--- services/MovieService.js
+++ services/MovieService.js
@@ -1,4 +1,11 @@
+const movies = require("../models/Movie");
+
 var movieService;
 movieService.addNewMovie=(data)=>{
+const movie=  movies.create({
+    name:data.name,
+    rating:data.rating,
+    releaseDate:data.releaseDate
+  })
   
 }
