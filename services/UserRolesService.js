412a67a9652fd6e34244314f51241ec38b84835e
kran891 <kranthi8919@gmail.com>
2023-12-12 17:34:25 +0530

    authentication n auth added

---
+++ services/UserRolesService.js
@@ -0,0 +1,31 @@
+const userRoles = require("../models/UserRoles");
+const rolesService = require("./RolesService");
+
+var  userRolesService={}
+userRolesService.addUserRoles= async(userId,roles)=>{
+    let roleid
+    roles.forEach(async element => {
+        roleid=await rolesService.findRoleByName(element)
+        if(!roleid){
+            roleid=await rolesService.addNewRole(element)
+        }
+       await userRolesService.addUserRole(userId,roleid)
+    });
+}
+userRolesService.addUserRole=async(userId,role)=>{
+    const userrole=new userRoles({
+        userId:userId,
+        roleId:role
+    })
+    await userrole.save();
+}
+userRolesService.getAllRoles=async (userId)=>{
+  const roles=await userRoles.find({userId:userId},{roleId:1}).populate("roleId");
+  const data=[]
+  roles.forEach(element => {
+    
+    data.push(element.roleId.role)
+  });
+return data
+}
+module.exports=userRolesService;
