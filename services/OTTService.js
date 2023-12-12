af2cbe7c12b78d00c298f6f7bc258dc31c785eaf
Naveen <bsa.naveenbabu@gmail.com>
2023-12-12 10:59:40 +0530

    OTT service added

---
+++ services/OTTService.js
@@ -0,0 +1,20 @@
+const otts = require("../models/OTT");
+
+var ottService;
+
+ottService.addOTT = async (name) =>{
+    const ott = await otts.create({name : name})
+    return ott._id;
+};
+ottService.updateOTT = async (name) => {
+    let ott = await otts.findOne(name);
+    ott.name = name;
+    await otts.updateOne(ott);
+    return ott._id;
+}
+ottService.deleteOTT = async (name) => {
+    let ott = await otts.findOne(name);
+    await otts.deleteOne(ott);
+    return otts._id;
+}
+
